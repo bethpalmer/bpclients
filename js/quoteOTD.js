8488373e2a4415fe61f0b6c039ba26df232a06c4
Beth Palmer <bethpalmerdesigns@gmail.com>
2017-07-10 23:22:34 +0100

    Added 2 new logo projects and changed about me text significantly

--- js/quoteOTD.js
+++ js/quoteOTD.js
@@ -1,1 +1,2 @@
 $("#otd").append('QUOTE OTD : "Dreams and Dedication are a Powerful Combination." <em>William Longgood</em>');
+// 'QUOTE OTD : "Do the work that feeds your soul, not your ego." <em>Jessica Walsh</em>'
